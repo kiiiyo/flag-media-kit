56b1dbad0671a2450247510c033ad1d96721dec6
kiyota.takashi <kiyota.takashi@classmethod.jp>
2022-04-20 18:33:29 +0900

    :recycle: modify refactoring code

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>🏴 Flag Media Kit</title>
+        <meta name="description" content="Jamstack user interface kit." />
+      </Head>
+      <div>Home Page</div>
+    </div>
+  );
+};
+
+export default Home;
